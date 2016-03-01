4209da3bccfbac3b44635fb93e2e8b9c9aff58cc
Sander <hanakmumakil23@gmail.com>
2016-03-01 13:55:31 +0100

    Adds Log-tab

---
+++ src/exerciseLogging/mainController.java
@@ -0,0 +1,27 @@
+package exerciseLogging;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+/**
+ * Created by Sander on 01/03/2016.
+ */
+public class mainController {
+
+    @FXML
+    public void showExercises() {
+        System.out.println("exercises");
+    }
+
+    @FXML
+    public void showTemplates() {
+    }
+
+    @FXML
+    public void showLog() {
+    }
+
+    @FXML
+    public void showHistory() {
+    }
+}
