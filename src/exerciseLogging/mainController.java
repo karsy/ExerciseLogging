f01f7cad7df4505dbca9aa137dc4ca65162b7c6f
Sander <hanakmumakil23@gmail.com>
2016-03-03 10:23:11 +0100

    Add example-functionality to history tabs with prompt

--- src/exerciseLogging/mainController.java
+++ src/exerciseLogging/mainController.java
@@ -3,13 +3,19 @@ package exerciseLogging;
 import javafx.beans.value.ChangeListener;
 import javafx.collections.ObservableArray;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.SortedList;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ListView;
 import javafx.scene.control.RadioButton;
 
 
 public class mainController{
+
+    // test-lists for testing with tests (TEST-TEST-TEST)
+    String[] list1 = {"hei", "på","deg"};
+    String[] list2 = {"bla", "la", "ta"};
 
     @FXML
     private ListView historySelectListView;
@@ -29,29 +35,43 @@ public class mainController{
         historyByExerciseRadioButton.setOnAction(this::radioButtonSwitching);
 
 
-        historySelectListView.getSelectionModel().selectionModeProperty().addListener((observable -> {
-            // TODO: make a class for all objects to get their logged workouts or implement the above method with JDBC?
+        historySelectListView.getSelectionModel().selectedItemProperty().addListener((observable -> {
+            historyLoggedListView.getItems().clear();
+            historyLoggedListView.getItems().addAll(historySelectListView.getSelectionModel().getSelectedItem().toString());
+            // TODO: make a class for all objects to get their logged workouts or implement the above method with JDBC
+        }));
+
+        historyLoggedListView.getSelectionModel().selectedItemProperty().addListener((observable -> {
+            // TODO: make this alert get the appropriate information from selected workout/exercise.
+            Alert workoutAlert = new Alert(Alert.AlertType.INFORMATION);
+            workoutAlert.setGraphic(null);
+            workoutAlert.setTitle("Workout title");
+            workoutAlert.setHeaderText("Workout on date: 1234567");
+            workoutAlert.setContentText("*All the fucking data from this workout that we want to display.*");
+            workoutAlert.showAndWait();
         }));
     }
 
     @FXML
     public void historyByWorkout(){
         // TODO: change historySelectListView to be fed with workouts
+        historySelectListView.getItems().clear();
+        historySelectListView.getItems().addAll(list1);
     }
 
     @FXML
     public void historyByExercise(){
         // TODO: change historySelectListView to be fed with exercises
+        historySelectListView.getItems().clear();
+        historySelectListView.getItems().addAll(list2);
     }
 
     @FXML
     public void displayLoggedHistory(boolean historyByWorkout){
         // TODO: change items in historyLoggedListView according to selection from historySelectListView
         if(historyByWorkout){
-            System.out.println("DISPLAYING LOGGED WORKOUTS BY WORKOUT");
             historyByWorkout();
         } else {
-            System.out.println("DISPLAYING LOGGED WORKOUTS BY EXERCISE");
             historyByExercise();
         }
     }
