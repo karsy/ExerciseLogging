2377b12af2a2d3535d71bb46088654d535317b95
karsy <vegard.karstang@gmail.com>
2016-03-07 14:01:07 +0100

    Start working on templates.

--- src/exerciseLogging/templatesController.java
+++ src/exerciseLogging/templatesController.java
@@ -1,5 +1,26 @@
 package exerciseLogging;
 
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class templatesController {
+
+    @FXML
+    private TextField descField;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    private void saveTemplate(ActionEvent event) {
+        String description = descField.getText();
+        String name = nameField.getText();
+
+    }
 }
