01b92e2963a4371c5d9169835bf0610e9665c3d5
karsy <vegard.karstang@gmail.com>
2016-03-14 10:56:14 +0100

    Added fxml variables.

--- src/exerciseLogging/templatesController.java
+++ src/exerciseLogging/templatesController.java
@@ -4,6 +4,7 @@ package exerciseLogging;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
 public class templatesController {
@@ -18,9 +19,19 @@ public class templatesController {
     private Button saveButton;
 
     @FXML
+    private ListView exercises;
+
+    @FXML
+    private ListView selectedExercises;
+
+    @FXML
+    private ListView templateList;
+
+    @FXML
     private void saveTemplate(ActionEvent event) {
         String description = descField.getText();
         String name = nameField.getText();
-
     }
+
+    
 }
